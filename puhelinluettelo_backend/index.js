8327883a23e4ab13b0b2b4e6dc7cafbed9fde236
janjok <118051663+janjok@users.noreply.github.com>
2026-01-29 17:49:14 +0200

    o3 t3.2

--- puhelinluettelo_backend/index.js
+++ puhelinluettelo_backend/index.js
@@ -28,6 +28,12 @@ app.get('/api/persons/', (request, response) => {
     response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+    const date = Date()
+
+    response.send(`Phonebook has info for ${persons.length} people <br/> ${date}`)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
