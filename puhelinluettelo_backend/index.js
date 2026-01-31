8792afd350cd92d58799e1737b769eb6b098f381
janjok <118051663+janjok@users.noreply.github.com>
2026-01-31 10:12:12 +0200

    o3 t3.7 && t3.8

--- puhelinluettelo_backend/index.js
+++ puhelinluettelo_backend/index.js
@@ -1,7 +1,13 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
+morgan.token('body', request => {
+    return JSON.stringify(request.body)
+})
+
 app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
     {
@@ -84,8 +90,6 @@ app.post('/api/persons/', (request, response) => {
         persons = persons.concat(person)
         response.json(person)
     }
-
-    
 })
 
 const PORT = 3001
