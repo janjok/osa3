d581862b0e64a956457f34cdf6de1b45b94fbcbc
janjok <118051663+janjok@users.noreply.github.com>
2026-02-02 11:01:16 +0200

    add dist

--- puhelinluettelo_backend/index.js
+++ puhelinluettelo_backend/index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
+
+app.use(cors())
+app.use(express.static('dist'))
 
 morgan.token('body', request => {
     return JSON.stringify(request.body)
@@ -92,7 +96,7 @@ app.post('/api/persons/', (request, response) => {
     }
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
