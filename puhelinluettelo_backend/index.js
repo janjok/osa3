9eea60d74b7808be4d06da5a861638860a5a48ea
janjok <118051663+janjok@users.noreply.github.com>
2026-02-12 15:40:11 +0200

    test for render

--- puhelinluettelo_backend/index.js
+++ puhelinluettelo_backend/index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const morgan = require('morgan')
-const mongoose = require('mongoose')
 const app = express()
 const Person = require('./models/mongo')
 
--- puhelinluettelo_backend/models/mongo.js
+++ puhelinluettelo_backend/models/mongo.js
@@ -32,7 +32,15 @@ if(process.argv.length === 3) {
     
 }
 
-const password = process.argv[2]
+personSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+const password = process.env.PASSWORD
 
 const url = `mongodb+srv://fullstack:${password}@cluster0.etwnlux.mongodb.net/phonebookApp?retryWrites=true&w=majority&appName=Cluster0`
 
